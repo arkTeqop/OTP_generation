15185d740c9e705b9b0f9ab149398611f537713e
ark786 <ark.sri.vas.tav@gmail.com>
2020-09-08 17:51:39 +0530

    verify part

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
 import styles from './styles';
 import axios from 'axios'
 export default function App() {
-  const [reply, setReply] = React.useState({});
+  const [otp, setotp] = React.useState({});
 
-  componentDidMount = () => {
-    axios.get('https://dev.splitsub.in/api/user/loginOtp')
+  const getOTP = () => { 
+    axios.post('https://dev.splitsub.in/api/user/loginOtp',{countryCode:"+91",username:"8377035582"})
     .then(res=>{console.log(res)})
 
+  }
+  const verifyOTP = () => { 
+    
+    axios.get('https://dev.splitsub.in/api/user/loginOtp',{username:"8377035582",countryCode:"+91",otp:otp})
+    .then(console.log(otp)) //res=>{console.log(res)})
+    .catch(error=>
+      console.log(error));
 
   }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title={"try"} onPress={componentDidMount}/>
+      <Button title={"try"} onPress={getOTP}/>
+      <TextInput placeholder="OTP" onChangeText={setotp}/>
+      <Button title="verify" onPress={verifyOTP}/>
+
       <StatusBar style="auto" />
     </View>
   );
