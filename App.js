9cd69f25dd03726f56124e35718ca5762491faba
ark786 <ark.sri.vas.tav@gmail.com>
2020-09-08 16:03:18 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View, Button } from 'react-native';
+import styles from './styles';
+import axios from 'axios'
 export default function App() {
+  const [reply, setReply] = React.useState({});
+
+  componentDidMount = () => {
+    axios.get('https://dev.splitsub.in/api/user/loginOtp')
+    .then(res=>{console.log(res)})
+
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"try"} onPress={componentDidMount}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
